51b5c025f48696ff06be96ee05496adf3b563a8f
carlosismaelad <carlosismaelad@outlook.com>
2024-10-25 09:29:04 -0300

    Add '/' route

--- src/webserver/ports/fastify/index.ts
+++ src/webserver/ports/fastify/index.ts
@@ -1,15 +1,22 @@
 import "dotenv/config";
 import fastify from "fastify";
+import { HTTPRequestPart } from "fastify/types/request";
+import { Http2ServerResponse } from "http2";
 
 const server = fastify({ logger: true });
 
 const app = async () => {
+  server.get("/", async (request, resposnse) => {
+    return "API ok!";
+  });
   try {
     server.listen({
       host: "0.0.0.0",
       port: process.env.PORT_SERVER ? Number(process.env.SERVER_PORT) : 5000,
     });
-    console.log(`🟢 Server Fastify is running on Port ${5000}!`);
+    console.log(
+      `🟢 Server Fastify is running on Port ${process.env.SERVER_PORT}!`,
+    );
   } catch (error) {
     console.log("Error server fastify: ", error);
   }
