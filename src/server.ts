4abdd724c1108d798cab2784be74d17dfbb2f88b
Carlos Dourado <carlosismaelad@outlook.com>
2024-10-27 19:04:24 -0300

    Adds database connection to server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
+import { getNewClient } from "../src/infra/database/connection.js";
 import "dotenv/config";
 
-const fastify = "./infra/webserver/ports/fastify";
-const express = "./infra/webserver/ports/express";
+const fastify = "./infra/http/ports/fastify";
+const express = "./infra/http/ports/express";
 const server = process.env.SERVER_TYPE === "fastify" ? fastify : express;
 
-require(server);
+async function startServer() {
+  try {
+    const client = await getNewClient();
+    console.log("Servidor iniciando...");
+  } catch (error) {
+    console.log("Erro ao tentar conexão com o banco de dados: ", error);
+    process.exit(1);
+  }
+  require(server);
+}
+
+startServer();
