f4a7854c2fb6d6e7a7cf76e2aa137e954fcc307f
Carlos Dourado <carlosismaelad@outlook.com>
2024-10-27 20:19:10 -0300

    Rename read to readAll and adds readById method

--- src/domain/repository/IRepository.ts
+++ src/domain/repository/IRepository.ts
@@ -1,6 +1,7 @@
 export interface IRepository<T> {
   createItem(item: T): Promise<T>;
-  read(id?: string): Promise<T | T[]>;
+  readAll(): Promise<T[]>;
+  readById(id?: string): Promise<T>;
   update(id: string, item: T): Promise<T | null>;
   softDelete(id: string): Promise<void>;
 }
