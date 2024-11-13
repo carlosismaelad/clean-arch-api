83de4927bf093b39aab6a82ed2237381b330f358
carlosismaelad <carlosismaelad@outlook.com>
2024-11-13 11:28:56 -0300

    fix: changing methods in the /migrations route so that the treatment is done in migrationsController

--- src/infrastructure/http/routes/v1/migrationsRoutes.ts
+++ src/infrastructure/http/routes/v1/migrationsRoutes.ts
@@ -2,11 +2,7 @@ import { FastifyInstance } from "fastify";
 import migrations from "../../../../application/controllers/v1/migrations/migrationsController"; // Ajuste o caminho conforme necessário
 
 export default async function migrationsRoutes(app: FastifyInstance) {
-  app.get("/migrations", async (request, reply) => {
-    return migrations(request, reply);
-  });
-
-  app.post("/migrations", async (request, reply) => {
+  app.all("/migrations", async (request, reply) => {
     return migrations(request, reply);
   });
 }
